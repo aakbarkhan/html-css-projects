9996bd7540d5536aa3be4914dedf409dec49fdc3
aakbarkhan <aakuukhan@gmail.com>
2019-06-17 11:30:20 +0530

    updates js

---
+++ demmoosat/JavaScript OwnPro/main.js
@@ -0,0 +1,52 @@
+//
+var secretNumber=7;
+var guess=Number(prompt("guess a number"));
+if (guess>secretNumber){
+    alert("Your number is too high! try again!");
+}
+else if (guess<secretNumber){
+    alert("Your number is too low! try again!");
+}
+else {
+    alert("You are Right"); 
+}
+
+
+// var secretNumber=7;
+// var guess=Number(prompt("guess a number"));
+// switch(guess)
+// {
+//     case 
+// }
+
+//loops
+// var ans=prompt("are we there yet!");
+// while(ans !=="yes" && ans !=="yeah"){
+//    var ans=prompt("are we there yet!")
+// }
+// alert("yay! we made it!");
+
+// var ans=prompt("are we there yet!");
+// while(ans.indexOf("yes") ===-1 && ans.indexOf("yeah")===-1){
+//    var ans=prompt("are we there yet!")
+// }
+// alert("yay! we made it!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ demmoosat/script.js
@@ -0,0 +1,3 @@
+let day=prompt("How old are you?");
+ var days=day*365.25;
+alert(`${day}  years is approximately ${days} number of days you live!`);
